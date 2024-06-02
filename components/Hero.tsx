f72efc61995b8d0600ce268608ddf3e8e2d0affc
Josh Shire <joshshirechildren@gmail.com>
2024-06-02 14:26:56 -0600

    first commit

---
+++ components/Hero.tsx
@@ -0,0 +1,76 @@
+'use client'
+import { Spotlight } from './ui/Spotlight'
+import { AuroraBackground } from './ui/aurora-background';
+import { motion } from "framer-motion";
+import { FaLocationArrow } from "react-icons/fa";
+import { TextGenerateEffect } from './ui/text-generate-effect';
+import { Meteors } from './ui/meteors';
+import MagicButton from './ui/MagicButton';
+const Hero = () => {
+  return (
+    <div className="pb-20 pt-36 h-screen">
+        <div>
+        <AuroraBackground className='top-0 left-0 h-screen absolute w-screen '>
+          <motion.div
+            initial={{ opacity: 0.0, y: 40 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{
+              delay: 0.3,
+              duration: 0.8,
+              ease: "easeInOut",
+            }}
+            className="relative flex flex-col gap-4 items-center justify-center px-4 bg-black-100"
+          >
+          </motion.div>
+        </AuroraBackground>
+        
+        <Spotlight
+            className="top-0 left-60 h-screen w-screen hidden xl:flex"
+            fill="white"
+          />
+          
+          
+          <Spotlight
+            className="top-28 left-80 h-[80vh] w-[50vw] hidden xl:flex"
+            fill="blue"
+          />
+      </div>
+      <Meteors number={80} className='sm:flex hidden'/>
+      <div
+        className="h-screen w-full dark:bg-transparent bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
+       absolute top-0 left-0 flex items-center justify-center"
+      >
+        {/* Radial gradient for the container to give a faded look */}
+        <div
+          className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-transparent
+         bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
+        />
+      </div>
+      
+
+      <div className="flex justify-center relative my-20 z-10">
+
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+            
+            Dynamic Web Magic with Next.js
+          </p>
+          <h2>
+            <TextGenerateEffect words="Transforming Concepts into Seamless Experiences" className='text-center font-Montserrat text-[40px] md:text-5xl lg:text-6xl'/>
+          </h2>
+
+          <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl font-Montserrat'>
+            Hi, I'm Josh, a Next.js Developer Based in Canada
+          </p>
+            <MagicButton title='View My Work!' link='#about' position='right' icon={<FaLocationArrow/>} otherClasses='gap-3'/>
+
+            
+
+          
+      </div>
+     </div>
+    </div>
+  );
+}
+
+export default Hero
---
+++ components/ui/MagicButton.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+type Buttontypes = {
+    title?: string
+    link?: string
+    icon?: React.ReactNode
+    position: 'left' | 'right'
+    handleClick?: () => void
+    otherClasses?: string
+    
+}
+const MagicButton = ({title, link, icon,
+    position,
+    handleClick,
+    otherClasses}: Buttontypes) => {
+  return (
+    <a href={link}>
+    <button className="relative inline-flex h-12 overflow-hidden rounded-md p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 mt-5">
+            <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+            <span className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-md bg-black py-1 text-sm font-medium text-white backdrop-blur-3xl px-10 ${otherClasses}`}>
+                {position === 'left' ? icon : ''}
+              {title ? title : 'Click Me'}
+                {position === 'right' ? icon : ''}
+            </span>
+          </button>
+    </a>
+  )
+}
+
+export default MagicButton
